da75b2e6813f11f5e6ba255451e7be9d6f5c9c6b
Brandon Capparelli <brandoncappo@gmail.com>
2021-03-10 19:07:37 -0800

    test

---
+++ Fun Fitness/Fun Fitness/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fun Fitness
+//
+//  Created by Brandon Capparelli on 3/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,7 @@
+# project-group8
+project-group8 created by GitHub Classroom
+
+Francisco Ocegueda
+Keven Atalla
+Jimy Bombela
+Brandon Capparelli
