bbd3a43837ac72fcbe7d4f1bdfb0c534a2625bb6
Brandon Capparelli <brandoncappo@gmail.com>
2021-05-13 09:57:41 -0700

    Commit By Keven Atalla
    I am commiting Code for healthkit on Keven's behalf. This code is not yet implemented.

--- Fun_Fitness/Fun_Fitness/Calculations 2.swift
+++ Fun_Fitness/Fun_Fitness/Calculations 2.swift
@@ -2,88 +2,108 @@
 //  Calculations.swift
 //  Fun Fitness
 //
-//  Created by Brandon Capparelli on 3/22/21.
+//  Created by Kevin Atalla on 5/6/21.
 //
 
 import Foundation
-//
-//struct BMI
-//{
-//
-//    var height: Double
-//    var weight: Double
-//    var bmi: Double
-//    var age: Double
-//    var personbmr: Double
-//    var male: Bool
-//    var daysofExerc: Int
-//    var dailyCal: Double
-//    var workoutcals: Double
-//    var exWeight: Double
-//
-//    init(_ height: Double, _ weight: Double, _ age: Double, _ daysofExerc: Int, _ dailyCal: Double, _ personbmr: Double, _male: Bool, _ workoutcals: Double, _ exWeight: Double)
-//    {
-//        self.height = height
-//        self.weight = weight
-//        self.age = age
-//        self.dailyCal = dailyCal
-//        self.daysofExerc = daysofExerc
-//        self.personbmr = personbmr
-//        self.male = false
-//        self.workoutcals = workoutcals
-//        self.exWeight = exWeight
-//
-//        bmi = weight/(height*height)
-//    }
-//
-//    func display() -> String
-//    {
-//        return "\(bmi)"
-//
-//    }
-//    mutating func bmr() -> Double
-//    {
-//
-//        if male == true {
-//            personbmr =   66 + (6.3 * weight) + (12.9 * height) - (6.8 * age)
-//        }
-//        else if male == false {
-//            personbmr = 655 + (4.3 * weight) + (4.7 * height) - (4.7 * age)
-//    }
-//
-//        return personbmr
-//    }
-//    mutating func dailyCalories() -> Double {
-//        switch daysofExerc{
-//        case 1...3:
-//         dailyCal = personbmr * 1.375
-//            return dailyCal
-//        case 3...5:
-//            dailyCal = personbmr * 1.55
-//            return dailyCal
-//        case 6...7:
-//        dailyCal = personbmr * 1.725
-//        return dailyCal
-//        default: dailyCal = personbmr * 1.2
-//           return dailyCal
-//
-//        }
-//    }
-//    // calorie intakes based on http://www.checkyourhealth.org/eat-healthy/cal_calculator.php
-//
-//
-//    mutating func workoutcalc()  -> Double
-//    {
-//        var weightconv: Double
-//        weightconv = weight * 3500
-//        exWeight = (weightconv - (workoutcals) ) * 3500 // Expected Weight = Weight (in calories) - (total weekly workout calories) multiplied by 3500 to return weight in lbs
-//        return exWeight
-//    }
-//
-//
-//
-//}
-//
-//
-//let defaults = UserDefaults.standard
-//// https://youtu.be/U6odvRpv6SA https://www.youtube.com/watch?v=XzWBT6lIB3A
+import HealthKit
+
+class HealthKit {
+    private enum HealthkitSetupError: Error {
+        case notAvailableOnDevice
+        case dataTypeNotAvailable
+    }
+
+    class func authorizeHealthKit(completion: @escaping (Bool, Error?) -> Swift.Void) {
+        guard HKHealthStore.isHealthDataAvailable() else {
+            completion(false, HealthkitSetupError.notAvailableOnDevice)
+            return
+        }
+
+        guard let dateOfBirth = HKObjectType.characteristicType(forIdentifier: .dateOfBirth),
+              let bloodType = HKObjectType.characteristicType(forIdentifier: .bloodType),
+              let biologicalSex = HKObjectType.characteristicType(forIdentifier: .biologicalSex),
+              let bodyMassIndex = HKObjectType.quantityType(forIdentifier: .bodyMassIndex),
+              let height = HKObjectType.quantityType(forIdentifier: .height),
+              let bodyMass = HKObjectType.quantityType(forIdentifier: .bodyMass),
+              let stepCount = HKObjectType.quantityType(forIdentifier: .stepCount),
+              let activeEnergy = HKObjectType.quantityType(forIdentifier: .activeEnergyBurned)
+        else {
+            completion(false, HealthkitSetupError.dataTypeNotAvailable)
+            return
+        }
+
+        let healthKitTypesToWrite: Set<HKSampleType> = [bodyMassIndex,
+                                                        activeEnergy,
+                                                        HKObjectType.workoutType()]
+
+        let healthKitTypesToRead: Set<HKObjectType> = [
+            stepCount,
+            dateOfBirth,
+            bloodType,
+            biologicalSex,
+            bodyMassIndex,
+            height,
+            bodyMass,
+            HKObjectType.workoutType(),
+        ]
+
+        HKHealthStore().requestAuthorization(toShare: healthKitTypesToWrite,
+                                             read: healthKitTypesToRead) { success, error in
+            completion(success, error)
+        }
+    }
+}
+
+extension Date {
+    static func mondayAt12AM() -> Date {
+        return Calendar(identifier: .iso8601).date(from: Calendar(identifier: .iso8601).dateComponents([.yearForWeekOfYear, .weekOfYear], from: Date()))!
+    }
+}
+
+class HealthStore {
+    var healthStore: HKHealthStore?
+    var query: HKStatisticsCollectionQuery?
+    init() {
+        if HKHealthStore.isHealthDataAvailable() {
+            healthStore = HKHealthStore()
+        }
+    }
+
+    func calculateSteps(completion: @escaping (HKStatisticsCollection?) -> Void) {}
+
+    func requestAuthorization(completion: @escaping (Bool) -> Void) {
+        let stepType = HKQuantityType.quantityType(forIdentifier: HKQuantityTypeIdentifier.stepCount)!
+
+        let startDate = Calendar.current.date(byAdding: .day, value: -7, to: Date())!
+
+        let anchorDate = Date.mondayAt12AM()
+        let daily = DateComponents(day: 1)
+
+        let predicate = HKQuery.predicateForSamples(withStart: startDate, end: Date(), options: .strictStartDate)
+
+        query = HKStatisticsCollectionQuery(quantityType: stepType, quantitySamplePredicate: predicate, options: .cumulativeSum, anchorDate: anchorDate, intervalComponents: daily)
+
+        query!.initialResultsHandler = { query, statisticsCollection, _ in completion(statisticsCollection != nil)
+
+            if let healthStore = self.healthStore, let query = self.query {
+                healthStore.execute(query)
+            }
+        }
+
+        guard let healthStore = self.healthStore
+        else {
+            return completion(false)
+        }
+
+        healthStore.requestAuthorization(toShare: [], read: [stepType]) {
+            success, _ in completion(success)
+        }
+    }
+}
+
+struct Step: Identifiable {
+    let id = UUID()
+    let count: Int
+    let date: Date
+}
